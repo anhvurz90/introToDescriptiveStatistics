b327c4651acf0a426ede47e8f0a58631cc3200e2
anhvurz90 <anhvurz90@gmail.com>
2017-05-03 22:24:40 +0700

    Add Lesson1: Intro to Research Methods

---
+++ tutorials/tutorial.java
@@ -0,0 +1,28 @@
+	INTRODUCTION TO DESCRIPTIVE STATISTICS:
+
+Lesson1: Intro to Research Methods: {
+	01.Lauren's Intro Video:
+	02.Quiz: Believe Results: {
+		How many people I surveyed
+		Who I surveyed
+		How the survey was conducted
+	}
+	03.Quiz: Measure Memory: {
+		How would you measure memory?
+	}
+	04.Define Constructs: 
+	05.BBC Memory Test:
+	06.Memory Test Description:
+	07.BBC Scores:
+	08.Google Accounts:
+	09.Quiz: BBC Measurement:
+	10.Operational Definition:
+	11.Quiz: Constructs
+	12.Quiz: Operational Definition
+	13.Data:
+		Hours slept
+		Recognition Score
+		Temporal Memory Score
+	14.Quiz: Sleep and Memory
+	15.Quiz: Influence Memory
+}
