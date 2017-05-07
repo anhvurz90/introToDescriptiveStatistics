294bad2ea74034049c93777c6e51c26ccfd2fcd8
anhvurz90 <anhvurz90@gmail.com>
2017-05-07 17:24:34 +0700

    Continue Lesson1: Intro to Research Methods: 16->21

--- tutorials/tutorial.java
+++ tutorials/tutorial.java
@@ -25,4 +25,44 @@ Lesson1: Intro to Research Methods: {
 		Temporal Memory Score
 	14.Quiz: Sleep and Memory
 	15.Quiz: Influence Memory
+	16.Quiz: Control for Time of Day: {
+		Would you trust this data more if you know everyone had taken the test
+		at the same time of day? Yes
+		- Extraneous Factors (Lurking variable)
+	}
+	17.Quiz: Same score: {
+		- Population parameter: mu
+		- Sample statistics: x-bar
+		- Sampling error: mu - x-bar
+	}
+	18.Quiz: Sample Average:
+	19.Quiz: Better Sample: {
+		- Question: What will make the sample statistic 
+		closer to the population parameter? ---> Bigger sample
+	}
+	20.Randomness: {
+		- Perhaps the best typical example that we can give of 
+		the scientific meaning of random distribution is afforded 
+		by the arrangement of the drops of rain in a shower. 
+		No one can give a guess whereabouts at any instant 
+		a drop will fall, but we know that if we put out a sheet 
+		of paper it will gradually become uniformly spotted over;
+		and that if we were to mark out any two equal areas on 
+		the paper these would gradually tend to be struck equally often.
+					- John Venn, 1888
+		
+		---> random sample -> x-bar has chance to be the same as mu
+	}
+	21.Quiz: Visualize Relationship: {
+		- Ox: Hours slept
+			( independent variable or
+			 preditor variable)
+		- Oy: Temporal Memory Score
+			( dependent variable or
+			  outcome)
+		
+		- What can we say about the relationship between hours slept
+		and temporal memory score?
+		 ---> The more you sleep, the better your temporal memory score
+	}
 }
